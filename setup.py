94612a24a15b72227eab2e6b726bd0f4a43143f0
Moein Kareshk <mkareshk@outlook.com>
2024-04-27 20:48:51 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+
+setuptools.setup(
+    name="binimize",
+    version="0.0.1",
+    author="Moein Kareshk",
+    author_email="mkareshk@outlook.com",
+    description="QUBO Optimization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkareshk/binimize",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "torch"],
+    extras_require={"dev": ["pre-commit", "pytest", "pytest-runner"]},
+    python_requires=">=3.10",
+)
