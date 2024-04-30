0fc476c7d41fa151425a9c44e9949ccfa70316be
Moein Kareshk <mkareshk@outlook.com>
2024-04-29 22:51:35 -0700

    add dep for ga

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "torch", "pandas", "joblib"],
+    install_requires=["numpy", "torch", "pandas", "joblib", "pygad"],
     extras_require={"dev": ["pre-commit", "pytest", "pytest-runner", "pytest-cov"]},
     python_requires=">=3.10",
 )
